6a4028679c5f05ffe53e6c4c9b715b57d37f8031
MoDevIO <momo.tiltis@gmail.com>
2026-09-06 15:01:34 +0200

    feat: add pipewire airplay support

--- modules/nixos/services/pipewire.nix
+++ modules/nixos/services/pipewire.nix
@@ -7,5 +7,25 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    extraConfig.pipewire = {
+      "99-airplay-discover" = {
+        "context.modules" = [
+          {
+            name = "libpipewire-module-raop-discover";
+            args = { };
+          }
+        ];
+      };
+    };
+  };
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
   };
 }
