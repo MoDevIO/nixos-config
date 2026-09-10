2c52d7de75695d54cfc9a19580e070f320e3a35b
MoDevIO <momo.tiltis@gmail.com>
2026-09-10 21:13:35 +0200

    feat(hyprland): add touchpad gestures

--- modules/home-manager/hyprland/lua/input.lua
+++ modules/home-manager/hyprland/lua/input.lua
@@ -1,2 +1,5 @@
 hl.config({input = {touchpad = {natural_scroll = true}}})
 
+hl.gesture({fingers = 3, direction = "horizontal", action = "workspace"})
+hl.gesture({fingers = 3, direction = "down", action = "close"})
+hl.gesture({fingers = 3, direction = "up", action = "fullscreen"})
