802fce7bbf60029f03abe5e4a6fd3473f24a339d
MoDevIO <momo.tiltis@gmail.com>
2026-09-11 16:55:30 +0200

    style: run formatter on workspaces.lua

--- modules/home-manager/hyprland/lua/workspaces.lua
+++ modules/home-manager/hyprland/lua/workspaces.lua
@@ -4,20 +4,13 @@ local function apply_workspace_rules(monitor)
     for i = 1, 10 do
         local id = monitor.id * 10 + i
 
-        hl.workspace_rule({
-            workspace = tostring(id),
-            monitor = monitor.name
-        })
+        hl.workspace_rule({workspace = tostring(id), monitor = monitor.name})
     end
 end
 
-for index, monitor in ipairs(monitors) do
-    apply_workspace_rules(monitor)
-end
+for index, monitor in ipairs(monitors) do apply_workspace_rules(monitor) end
 
-hl.on("monitor.added", function(monitor)
-    apply_workspace_rules(monitor)
-end)
+hl.on("monitor.added", function(monitor) apply_workspace_rules(monitor) end)
 
 for i = 1, 10 do
     local key = i % 10
@@ -26,12 +19,12 @@ for i = 1, 10 do
     hl.bind("SUPER + " .. tostring(key), function()
         local monitor = hl.get_active_monitor()
         local id = monitor.id * 10 + workspaceIndex
-        hl.dispatch(hl.dsp.focus({ workspace = id }))
+        hl.dispatch(hl.dsp.focus({workspace = id}))
     end)
 
     hl.bind("SUPER + SHIFT + " .. tostring(key), function()
         local monitor = hl.get_active_monitor()
         local id = monitor.id * 10 + workspaceIndex
-        hl.dispatch(hl.dsp.window.move({ workspace = id }))
+        hl.dispatch(hl.dsp.window.move({workspace = id}))
     end)
 end
