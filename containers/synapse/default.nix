80661c211c5a1d7d22f8cf3879d217f9c8b886d3
MoDevIO <momo.tiltis@gmail.com>
2026-09-13 16:17:18 +0200

    feat(servre): implement synapse service including matrix-discord, mautrix-signal and mautrix-telegram

--- containers/synapse/default.nix
+++ containers/synapse/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   self,
   prefixLength,
   ipAddr,
@@ -6,10 +7,31 @@
 }:
 
 {
+  sops.secrets."tailscale_auth_key" = { };
+
+  sops.secrets."matrix/registration_shared_secret" = { };
+  sops.secrets."matrix/telegram_api_hash" = { };
+  sops.secrets."matrix/signal_pickle_key" = { };
+
+  sops.templates."mautrix-telegram.env".content = ''
+    MAUTRIX_TELEGRAM_TELEGRAM_API_HASH=${config.sops.placeholder."matrix/telegram_api_hash"}
+  '';
+  sops.templates."mautrix-signal.env".content = ''
+    MAUTRIX_SIGNAL_ENCRYPTION_PICKLE_KEY=${config.sops.placeholder."matrix/signal_pickle_key"}
+  '';
+
   containers.synapse = {
     autoStart = true;
     privateNetwork = true;
     hostBridge = "br0";
+    enableTun = true;
+
+    bindMounts = {
+      "/run/secrets" = {
+        hostPath = "/run/secrets";
+        isReadOnly = true;
+      };
+    };
 
     config = {
       imports = [
@@ -18,6 +40,17 @@
       _module.args = {
         inherit prefixLength ipAddr;
       };
+
+      networking.hostName = "synapse";
+
+      services.tailscale = {
+        enable = true;
+        authKeyFile = "/run/secrets/tailscale_auth_key";
+      };
+
+      nixpkgs.config.permittedInsecurePackages = [
+        "olm-3.2.16"
+      ];
 
       networking.firewall.enable = false;
 
@@ -43,6 +76,8 @@
         settings = {
           server_name = "motrix.click";
 
+          registration_shared_secret = config.sops.secrets."matrix/registration_shared_secret".path;
+
           listeners = [
             {
               port = 8008;
@@ -59,6 +94,69 @@
               ];
             }
           ];
+        };
+      };
+
+      services.mautrix-discord = {
+        enable = true;
+        registerToSynapse = true;
+        settings = {
+          homeserver = {
+            domain = "motrix.click";
+            address = "http://localhost:8008";
+          };
+          bridge = {
+            permissions."@mo:motrix.click" = "admin";
+
+            encryption = {
+              allow = true;
+              default = true;
+            };
+          };
+        };
+      };
+
+      services.mautrix-telegram = {
+        enable = true;
+        registerToSynapse = true;
+        environmentFile = config.sops.templates."mautrix-telegram.env".path;
+        settings = {
+          telegram.api_id = 39985518;
+          homeserver = {
+            domain = "motrix.click";
+            address = "http://localhost:8008";
+          };
+
+          bridge = {
+            permissions."@mo:motrix.click" = "admin";
+
+            encryption = {
+              allow = true;
+              default = true;
+            };
+          };
+        };
+      };
+
+      services.mautrix-signal = {
+        enable = true;
+        registerToSynapse = true;
+        environmentFile = config.sops.templates."mautrix-signal.env".path;
+        settings = {
+          homeserver = {
+            domain = "motrix.click";
+            address = "http://localhost:8008";
+          };
+
+          encryption = {
+            allow = true;
+            default = true;
+            pickle_key = "$MAUTRIX_SIGNAL_ENCRYPTION_PICKLE_KEY";
+          };
+
+          bridge = {
+            permissions."@mo:motrix.click" = "admin";
+          };
         };
       };
     };
