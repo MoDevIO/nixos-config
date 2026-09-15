72bbb112ff9505b0c25425897e7bcdd1e3d5e096
MoDevIO <momo.tiltis@gmail.com>
2026-09-15 15:35:10 +0200

    fix(server): add container imports to nixser1

--- hosts/servers/nixser1/default.nix
+++ hosts/servers/nixser1/default.nix
@@ -4,5 +4,8 @@
   imports = [
     ./hardware-configuration.nix
     "${self}/containers/synapse"
+    "${self}/containers/immich"
+    "${self}/containers/navidrome"
+    "${self}/containers/jellyfin"
   ];
 }
