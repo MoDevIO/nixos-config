651502134c7a11f36f6e108505d238f5f6cd757f
MoDevIO <momo.tiltis@gmail.com>
2026-09-15 15:35:44 +0200

    feat(server): add server container ssh config

--- modules/server/container-networking.nix
+++ modules/server/container-networking.nix
@@ -1,10 +1,20 @@
-{ prefixLength, ipAddr, ... }:
+{
+  prefixLength,
+  ipAddr,
+  containerIpAddr,
+  ...
+}:
 
 {
+  services.openssh.enable = true;
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDQGs63zr5X6vgRhUD6+gOWXYaBsvHDddH/RwnccbkEp momo.tiltis@gmail.com"
+  ];
+
   networking = {
     interfaces.eth0.ipv4.addresses = [
       {
-        address = "192.168.179.101";
+        address = containerIpAddr;
         inherit prefixLength;
       }
     ];
