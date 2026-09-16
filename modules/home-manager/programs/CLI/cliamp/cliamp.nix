58a2fad7bcd631f1efb5d7d8a07555b3546dc35d
MoDevIO <momo.tiltis@gmail.com>
2026-09-16 14:29:48 +0200

    feat(cliamp): add navidrome auth to config.toml

--- modules/home-manager/programs/CLI/cliamp/cliamp.nix
+++ modules/home-manager/programs/CLI/cliamp/cliamp.nix
@@ -1,15 +1,23 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   home.packages = [
     pkgs.cliamp
   ];
-  home.file.".config/cliamp/config.toml" = {
-    text = ''
+
+  sops.secrets."navidrome/password" = { };
+
+  sops.templates."cliamp-config.toml" = {
+    path = "${config.home.homeDirectory}/.config/cliamp/config.toml";
+    content = ''
       [spotify]
       enabled = true
       client_id = "83e1a2a15d9e42b9bfcd755cb686f7da"
+
+      [navidrome]
+        url = "http://192.168.179.103:4533"
+        user = "admin"
+        password = ${config.sops.placeholder."navidrome/password"}
     '';
-    force = true;
   };
 }
